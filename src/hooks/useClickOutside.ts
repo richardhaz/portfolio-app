5ba52934b2b8878ecd96d3603f24fe88f295e364
Richard Huaman <66971727+richardhaz@users.noreply.github.com>
2023-04-17 18:14:56 -0500

    remove mail service

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -18,5 +18,6 @@ export const useClickOutside = ({ ref, fn }: UseClickOutsideProps): void => {
       // Unbind the event listener on clean up
       document.removeEventListener('mousedown', handleClickOutside);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref]);
 };
